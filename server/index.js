848f7646b606b808646fcca2c6edccbd2ef28424
Priyanka Parikh <priyanka.parikh6@gmail.com>
2018-04-02 12:28:41 -0700

    added oauth

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const passport = require ('passport');
+const GoogleStrategy = require ('passport-google-oauth20').strategy
 const app = express()
+
+passport.use(new GoogleStrategy());
+// new GoogleStrategy creates a new instance of the passport GoogleStrategy
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there'});
