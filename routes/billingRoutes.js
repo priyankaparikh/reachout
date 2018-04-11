3180e3b77ecda1a34971279b6dd8ff9791c5fe45
Priyanka Parikh <priyanka.parikh6@gmail.com>
2018-04-11 13:58:21 -0700

    workign on debugging strip API

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,6 +3,7 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post('api/stripe', async (req, res) => {
+    console.log(req.body)
     const charge = await stripe.charges.create({
       amount:500,
       currency: 'usd',
